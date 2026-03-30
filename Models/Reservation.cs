1f78e6dbf1e0dc1b3b63d5996b9d6c717a94d06e
MagicJJ34 <milosz.makiel3@gmail.com>
2026-03-30 21:15:55 +0200

    Add entity models

---
+++ Models/Reservation.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace CourtBookingApp.Models
+{
+    public class Reservation
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int CourtId { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public enum Status 
+        {
+            Active,
+            Cancelled,
+            Completed
+        }
+        public DateTime CreatedAt { get; set; }
+
+        public User User { get; set; }
+        public Court Court { get; set; }
+    }
+}
