80ed724afbd38710ad6d0e79930db057ea0dbc3e
MagicJJ34 <milosz.makiel3@gmail.com>
2026-04-07 00:34:45 +0200

    Add Services

---
+++ Services/IReservationService.cs
@@ -0,0 +1,10 @@
+﻿using CourtBookingApp.Models;
+
+namespace CourtBookingApp.Services
+{
+    public interface IReservationService
+    {
+        Task<Reservation> CreateAsync(Reservation reservation);
+        Task<List<Reservation>> GetAllAsync();
+    }
+}
---
+++ Services/ReservationService.cs
@@ -0,0 +1,27 @@
+﻿using CourtBookingApp.Models;
+using CourtBookingApp.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace CourtBookingApp.Services
+{
+    public class ReservationService : IReservationService
+    {
+        private readonly  AppDbContext _context;
+        public ReservationService (AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<Reservation> CreateAsync(Reservation reservation)
+        {
+            _context.Reservations.Add(reservation);
+            await _context.SaveChangesAsync();
+            return reservation;
+        }
+
+        public async Task<List<Reservation>> GetAllAsync()
+        
+        {
+            return await _context.Reservations.ToListAsync();
+        }
+    }
+}
