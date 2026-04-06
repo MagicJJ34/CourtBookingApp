4f35403b9f27557e3a73ff7d5a201a4a14f4a588
MagicJJ34 <milosz.makiel3@gmail.com>
2026-04-07 00:32:46 +0200

    Add Data

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using CourtBookingApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CourtBookingApp.Data
+
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        { 
+        }
+
+        public DbSet<Reservation> Reservations { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Court> Courts { get; set; }
+    }
+}
