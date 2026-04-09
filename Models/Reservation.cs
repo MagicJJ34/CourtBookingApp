2d4951794c8cf04dc73ec5d3f97aea97a65937bd
MagicJJ34 <milosz.makiel3@gmail.com>
2026-04-09 08:02:49 +0200

    Improved reservation model

--- Models/Reservation.cs
+++ Models/Reservation.cs
@@ -6,7 +6,9 @@ namespace CourtBookingApp.Models
     {
         public int Id { get; set; }
         public int UserId { get; set; }
+        public User? User { get; set; }
         public int CourtId { get; set; }
+        public Court? Court { get; set; }
         public DateTime StartTime { get; set; }
         public DateTime EndTime { get; set; }
         public enum Status 
@@ -16,8 +18,5 @@ namespace CourtBookingApp.Models
             Completed
         }
         public DateTime CreatedAt { get; set; }
-
-        public User? User { get; set; }
-        public Court? Court { get; set; }
     }
 }
