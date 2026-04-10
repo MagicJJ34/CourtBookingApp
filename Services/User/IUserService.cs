fbdd45d00c6a89e48520f2a323cffd91f4e89c8c
MagicJJ34 <milosz.makiel3@gmail.com>
2026-04-11 00:29:02 +0200

    Add User service

---
+++ Services/User/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using CourtBookingApp.Models;
+
+namespace CourtBookingApp.Services;
+
+    public interface IUserService
+    {
+        Task<Users> CreateUserAsync(Users user);
+        Task<IEnumerable<Users>> GetAllAsync();
+        Task<Users> UpdateAsync(int id, Users updatedUser);
+        Task<Users> GetByIdAsync(int id);
+        Task<bool> DeleteAsync(int id);
+    }
---
+++ Services/User/UserServices.cs
@@ -0,0 +1,56 @@
+﻿using CourtBookingApp.Models;
+using CourtBookingApp.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using CourtBookingApp.Services.User;
+
+namespace CourtBookingApp.Services.User;
+
+public class UserService : IUserService
+{
+    private readonly AppDbContext _context;
+
+    public UserService(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Users>CreateUserAsync(Users user)
+    {
+        _context.Users.Add(user);
+        await _context.SaveChangesAsync();
+        return user;
+    }
+
+    public async Task<IEnumerable<Users>>GetAllAsync()
+    {
+        var data = await _context.Users.ToListAsync();
+        return data;
+    }
+
+    public async Task<Users>UpdateAsync(int id, Users updatedUser)
+    {
+        var user = await _context.Users.FindAsync(id);
+
+        user.Name = updatedUser.Name;
+        user.Email = updatedUser.Email;
+        user.PhoneNumber = updatedUser.PhoneNumber;
+
+        await _context.SaveChangesAsync();
+        return user;
+    }
+
+    public async Task<Users>GetByIdAsync(int id)
+    {
+        var user = await _context.Users.FindAsync(id);
+        return user;
+    }
+
+    public async Task<bool>DeleteAsync(int id)
+    {
+        var user = await _context.Users.FindAsync(id);
+        _context.Users.Remove(user);
+        await _context.SaveChangesAsync();
+        return true;
+    }
+}
