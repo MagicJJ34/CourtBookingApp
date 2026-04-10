e5dde011108591d2583e97fcfb9659ba9e195dee
MagicJJ34 <milosz.makiel3@gmail.com>
2026-04-11 00:28:28 +0200

    Rename

---
+++ Models/Users.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourtBookingApp.Models
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Role { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public ICollection<Reservation> Reservations { get; set; }
+
+    }
+}
