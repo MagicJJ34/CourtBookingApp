0b6b35dbf91cb927371612b9ccfde87564fff379
MagicJJ34 <milosz.makiel3@gmail.com>
2026-04-11 09:53:48 +0200

    Add Courts service

---
+++ Services/Courts/CourtServices.cs
@@ -0,0 +1,54 @@
+﻿using CourtBookingApp.Models;
+using CourtBookingApp.Data;
+using Microsoft.EntityFrameworkCore;
+using CourtBookingApp.Services.Courts;
+
+namespace CourtBookingApp.Services.Courts;
+
+
+public class CourtServices : ICourtService
+{
+    private readonly AppDbContext _context;
+
+    public CourtServices(AppDbContext context)
+    {
+        _context = context;
+    }
+    public async Task<Court> CreateCourtAsync(Court court)
+    {
+        _context.Courts.Add(court);
+        await _context.SaveChangesAsync();
+        return court;
+    }
+    public async Task<Court> UpdateAsync(int id, Court updatedCourt)
+    {
+        var court = await _context.Courts.FindAsync(id);
+
+        court.Name = updatedCourt.Name;
+        court.Type = updatedCourt.Type;
+        court.HasRoof = updatedCourt.HasRoof;
+        court.PricePerHour = updatedCourt.PricePerHour;
+
+        await _context.SaveChangesAsync();
+        return court;
+    }
+    
+    public async Task<IEnumerable<Court>>GetAllAsync()
+    {
+        var data = await _context.Courts.ToListAsync();
+        return data;
+    }
+
+    public async Task<Court> GetByIdAsync(int id)
+    {
+        var court = await _context.Courts.FindAsync(id);
+        return court;
+    }
+    public async Task<bool> DeleteAsync(int id)
+    {
+        var court = await _context.Courts.FindAsync(id);
+        _context.Courts.Remove(court);
+        await _context.SaveChangesAsync();
+        return true;
+    }
+}
---
+++ Services/Courts/ICourtService.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CourtBookingApp.Models;
+using System.Security.AccessControl;
+using CourtBookingApp.Data;
+
+namespace CourtBookingApp.Services.Courts
+{
+    public interface ICourtService
+    {
+        Task<Court> CreateCourtAsync(Court court);
+        Task<IEnumerable<Court>> GetAllAsync();
+        Task<Court> UpdateAsync(int id, Court updatedCourt);
+        Task<Court> GetByIdAsync(int id);
+        Task<bool> DeleteAsync(int id);
+    }
+}
