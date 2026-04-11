a9f7ac9ff8bf7cd2d28726adcb40e6e25c119e49
MagicJJ34 <milosz.makiel3@gmail.com>
2026-04-11 09:54:17 +0200

    Rename

---
+++ Models/Court.cs
@@ -0,0 +1,13 @@
+﻿namespace CourtBookingApp.Models
+{
+    public class Court
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public bool HasRoof { get; set; }
+        public int PricePerHour { get; set; }
+
+        public ICollection<Reservation> Reservations { get; set; }
+    }
+}
